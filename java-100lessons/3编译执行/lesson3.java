b3b785e9b55743bf1dfd258b8f71bcabd1c0dcd3
LOUSANPANG <1271255653@qq.com>
2023-01-14 19:31:16 +0800

    add: java

---
+++ java-100lessons/3编译执行/lesson3.java
@@ -0,0 +1,14 @@
+// class Demo {
+//     public static void main(String[] args) {
+// 	System.out.print("谈恋爱了！！！");
+// 	System.out.println("开心！！！");
+//     }
+// }
+
+// public 修饰符 强制要求类名和文件名一致
+public class lesson4 {
+    public static void main(String[] args) {
+	System.out.print("谈恋爱了！！！");
+	System.out.println("开心！！！");
+    }
+}
---
+++ java-100lessons/8语法结构/Test.java
@@ -0,0 +1,47 @@
+/**
+    顺序结构
+
+    分支结构
+        if分支
+        switch case
+            byte short int char enum String
+    
+    循环结构
+        for
+            for(; ;) {}
+        while
+        do while
+    
+    == 与 equals  xx.equals()
+ */
+
+ import java.util.Scanner;
+
+public class Test {
+    public static void main (String[] args) {
+        // Scanner input = new Scanner(System.in);
+        // System.out.println("请输入：");
+        // int month = input.nextInt();
+        // if (month == 1) {
+        //     System.out.println("已输入一");
+        // } else {
+        //     System.out.println("输入其他");
+        // }
+
+        // int a = 1;
+        // switch(a) {
+        //     case 1:
+        //         System.out.println("1");
+        //         break;
+        //     case 2:
+        //         System.out.println("2");
+        //         break;
+        //     default:
+        //         system.out.println("3");
+        // }
+
+        // for(int a=1; a<=5; a++) {
+        //     System.out.println(a);
+        // }
+    }
+}
