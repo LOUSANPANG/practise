2aa84d1a77d806c0876dcb330413e4a5315b0083
LOUSANPANG <1271255653@qq.com>
2023-01-14 19:31:49 +0800

    chore: remove main code

--- web/Axios_js/main.js
+++
@@ -1,9 +0,0 @@
-import Vue from 'vue'
-import './public/config'
-import api from '@/request/api'
-
-Vue.prototype.$api = api
-
-// 应用
-this.$api.user.userDetail(1, { test: 1 })
-  .then(res => {})
--- web/Axios_js/service/api/index.js
+++
@@ -1,5 +0,0 @@
-import user from '@/api/user' // 用户模块接口
-
-export default {
-  user
-}
