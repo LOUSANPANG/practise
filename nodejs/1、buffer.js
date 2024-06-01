7d53bf8c58fd74ddb8878cf8a8e95b336678c01d
LOUSANPANG <1271255653@qq.com>
2024-06-01 12:47:07 +0800

    feat: add node express mongodb

--- nodejs/1、buffer.js
+++
@@ -1,8 +0,0 @@
-// 创建
-Buffer.alloc(10);
-Buffer.allocUnsafe(10);
-Buffer.from([1, 2, 3]);
-
-// buffer转换
-const buf1 = Buffer.from([105,108,111,118,101,121,111,117]);
-buf1.toString()  // i love you
